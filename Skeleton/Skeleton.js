3068c778a23c5c3af7dbf54b585c16db848fbaf0
Young <fomagran6@naver.com>
2022-04-07 10:10:07 -0500

    Skeleton 완성!

---
+++ Skeleton/Skeleton.js
@@ -0,0 +1,27 @@
+const header = document.getElementById("header");
+const title = document.getElementById("title");
+const excerpt = document.getElementById("excerpt");
+const profile_img = document.getElementById("profile_img");
+const name = document.getElementById("name");
+const date = document.getElementById("date");
+
+const animated_bgs = document.querySelectorAll(".animated-bg");
+const animated_bg_texts = document.querySelectorAll(".animated-bg-text");
+
+setTimeout(getData, 2000);
+
+function getData() {
+  header.innerHTML =
+    '<img src="https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="" />';
+
+  title.innerHTML = "Lorem impsu dolor sit amet";
+  excerpt.innerHTML =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis officiis veritatis sint dolore incidunt, quidem recusandae itaque commodi eos adipisci.";
+  profile_img.innerHTML =
+    '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />';
+  name.innerHTML = "Fomgran";
+  date.innerHTML = "Apr 07, 2022";
+}
+
+animated_bgs.forEach((bg) => bg.classList.remove("animated-bg"));
+animated_bg_texts.forEach((text) => text.classList.remove("animated-bg-text"));
