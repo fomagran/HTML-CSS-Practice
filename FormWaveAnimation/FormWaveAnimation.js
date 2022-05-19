78cdc315698fe3a108a29ac69bbf59cbef1a088b
Young <fomagran6@naver.com>
2022-05-19 15:44:29 -0500

    Animated CountDown 완성

--- FormWaveAnimation/FormWaveAnimation.js
+++ FormWaveAnimation/FormWaveAnimation.js
@@ -1,8 +1,15 @@
-const labels = document.querySelectorAll('.form-control label')
+const labels = document.querySelectorAll(".form-control label");
 
-labels.forEach(label => {
-    label.innerHTML = label.innerText
-    .split('')
-    .map((letter,idx) => `<span style="transition-delay:${idx*50}ms">${letter}</span>`)
-    .join('')
-})
+screen.orientation.lock("landscape").then(function () {
+  alert("Landscape");
+});
+
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split("")
+    .map(
+      (letter, idx) =>
+        `<span style="transition-delay:${idx * 50}ms">${letter}</span>`
+    )
+    .join("");
+});
